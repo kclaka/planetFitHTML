805bd04a92fed6f2aa07777302be8f3d2aef494a
Kenny <kennyigbechi@gmail.com>
2022-05-24 21:01:11 -0700

    add customer modal for crud

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 const customerTable = document.getElementById("customertable")
 const customerForm = document.getElementById('addCustomer');
+const CRUDbutton = document.getElementById('submit');
+
+const test = document.getElementById('customerform');
+
+let selected_user = null
 
 const buildTable = function(tableID, data) {
-    console.log(data[0]["fname"])
     
     for(var values of data){
         let newRow = tableID.insertRow();
@@ -21,25 +25,84 @@ const buildTable = function(tableID, data) {
             newCell.appendChild(newText);
 
         }
+
+        let entity_id = Object.values(values);
         let newCell = newRow.insertCell();
-        let edit = document.createElement("a")
-        edit.href = "#"
-        edit.onclick = "updateCustomer('this.customerID')"
+        let edit = document.createElement("button")
+        edit.type = "button"
+        edit.className = "btn btn-outline-secondary"
+        edit.setAttribute("data-bs-toggle", "modal")
+        edit.setAttribute("data-bs-target", '#customerModal')
+        edit.onclick = function(){updateTableContent(entity_id)}
         edit.innerHTML = "Edit"
-        let newText = document.createTextNode(`<h1>Hello</h1>`);
         newCell.appendChild(edit);
 
-        let deleteCell = newRow.insertCell();
-        let deleteElement = document.createElement("a")
-        deleteElement.href = "#"
-        deleteElement.onclick = "deleteCustomer('this.customerID')"
+        
+
+       
+        let deleteElement = document.createElement("button")
+        deleteElement.type = "button"
+        deleteElement.className = "btn btn-outline-danger"
+        deleteElement.onclick = function(){deleteTableContent(entity_id)}
         deleteElement.innerHTML = "Delete"
-        deleteCell.appendChild(deleteElement);
+        newCell.appendChild(deleteElement);
     }
 
         
     
 }
+
+const updateTableContent = (entity_id) => {
+    document.getElementById('fname').value = entity_id[1];
+    document.getElementById('lname').value = entity_id[2];
+    document.getElementById('pronouns').value = entity_id[3];
+    document.getElementById('age').value = entity_id[4];
+    document.getElementById('customerAddress').value = entity_id[5];
+    document.getElementById('email').value = entity_id[6];
+    document.getElementById('membershipType').value = entity_id[7];
+    document.getElementById('hasActiveMembership').value = entity_id[8];
+
+    CRUDbutton.innerHTML = "Update"
+
+    selected_user = entity_id[0]
+    console.log(selected_user)
+
+    customerForm.addEventListener('submit', async function (e) {
+        e.preventDefault();
+        
+        const formData = new FormData(customerForm).entries()
+        console.log(formData)
+        const response = await fetch(`https://planetfitapi.azurewebsites.net/api/customers/${selected_user}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(Object.fromEntries(formData))
+        });
+    
+        const result = await response.json();
+    
+        console.log(result)
+    
+        const modalElement = document.getElementById("customerModal");
+        const modal = bootstrap.Modal.getInstance(modalElement);
+        modal.hide();
+        document.getElementById("addCustomer").reset()
+        
+    });
+    
+
+    
+}
+
+const deleteTableContent = (entity_id) => {
+    console.log(`Deleted Entity with ID ${entity_id}`)
+}
+
+const closeUserModal = () => {
+    
+}
+
+
+
 
 const fetchData = function(entity) {
     fetch(`https://planetfitapi.azurewebsites.net/api/${entity}`, {
@@ -59,20 +122,34 @@ const fetchData = function(entity) {
 };
 
 
-
-customerForm.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    const formData = new FormData(customerForm).entries()
+const postCustomer = () => {
+    customerForm.addEventListener('submit', async function (e) {
+        e.preventDefault();
+        
+        const formData = new FormData(customerForm).entries()
+        console.log(formData)
+        const response = await fetch('https://planetfitapi.azurewebsites.net/api/customers', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(Object.fromEntries(formData))
+        });
     
-    const response = await fetch('https://planetfitapi.azurewebsites.net/api/customers', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(Object.fromEntries(formData))
+        const result = await response.json();
+    
+        const modalElement = document.getElementById("customerModal");
+        const modal = bootstrap.Modal.getInstance(modalElement);
+        modal.hide();
+        document.getElementById("addCustomer").reset()
+        
     });
+}
 
-    const result = await response.json();
-    console.log(result)
-    console.log(formData)
-    });
+
+
+
+
+
+
+
 
 customerData = fetchData("customers")
